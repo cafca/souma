39d534a8fb2aaab033f43153f7594fc96d6861c7
Vincent Ahrend <nichte@gmail.com>
2013-05-06 12:32:43 +0200

    Fix module import

--- start_soma.py
+++ start_soma.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python
+import sys
+import os
+
+# Add parent directory of current working directory to path so the current
+# directory can be imported as a module (I have no idea why this is neccessary).
+cwd_parent_dir = os.sep.join(os.getcwd().split(os.sep)[:-1])
+sys.path.append(cwd_parent_dir)
 
 from soma import app, db
 from soma.synapse.models import Starmap
 from gevent.wsgi import WSGIServer
 from synapse import Synapse
-from set_hosts import test_host_entry
 from sqlalchemy.exc import OperationalError
 
 # Initialize database
@@ -15,11 +21,6 @@ except OperationalError:
     db.create_all()
     db.session.add(Starmap(app.config['SOMA_ID']))
     db.session.commit()
-
-if not test_host_entry:
-    app.logger.error("Please execute set_hosts.py with administrator privileges\
-        to allow access to Soma at http://app.soma/.")
-    quit()
 
 if app.config['USE_DEBUG_SERVER']:
     # flask development server
