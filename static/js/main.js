b9f07eb89046eea3c897fff46db067659fbad7fc
Vincent Ahrend <nichte@gmail.com>
2014-01-15 16:55:00 +0100

    Fix toggling 1up button state

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,7 @@
 $(".oneup").click(function() {
     var star_id = $(this).attr("id").substr(6);
     $.post("/s/"+star_id+"/1up", function(data) {
-        $("#oneup-"+star_id).toggleClass("btnPrimary");
+        $("#oneup-"+star_id).toggleClass("btn-primary");
+        $("#oneup-"+star_id).toggleClass("btn-inverse");
     });
 });
