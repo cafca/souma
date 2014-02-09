84bf345a079d46b6660b5bf3debf54b60f52cb7e
Vincent Ahrend <nichte@gmail.com>
2014-02-09 22:58:18 +0100

    Update requirements
    All requirements replaced with latest versions, I have not checked
    which incompatibilities, etc. are introduced with this yet (except for
    those in web_ui/forms.py)

--- web_ui/forms.py
+++ web_ui/forms.py
@@ -1,11 +1,12 @@
-from flask.ext.wtf import Form, TextField, SelectField, FileField, HiddenField,\
-    Required, Optional, Length, Email, widgets
-from flask.ext.wtf.html5 import URLField
+from flask.ext.wtf import Form
+from wtforms.fields import TextField, TextAreaField, SelectField, FileField, HiddenField
+from wtforms.fields.html5 import URLField
+from wtforms.validators import DataRequired, Email
 
 
 class Create_persona_form(Form):
     """ Generate form for creating a persona """
-    name = TextField('Name', validators=[Required(), ])
+    name = TextField('Name', validators=[DataRequired(), ])
     email = TextField('Email (optional)', validators=[Email(), ])
 
 
@@ -13,8 +14,8 @@ class Create_star_form(Form):
     """ Generate form for creating a star """
     # Choices of the author field need to be set before displaying the form
     # TODO: Validate author selection
-    author = SelectField('Author', validators=[Required(), ])
-    text = TextField('Content', validators=[Required(), ], widget=widgets.TextArea())
+    author = SelectField('Author', validators=[DataRequired(), ])
+    text = TextAreaField('Content', validators=[DataRequired(), ])
     picture = FileField('Picture')
     #link = URLField('Link', validators=[url()])
     link = URLField('Link')
@@ -24,16 +25,16 @@ class Create_star_form(Form):
 class Create_group_form(Form):
     """ Generate form for creating a group """
 
-    author = HiddenField('Author', validators=[Required(), ])
-    groupname = TextField('Group name', validators=[Required(), ])
-    description = TextField('Description', validators=[Required(), ], widget=widgets.TextArea())
+    author = HiddenField('Author', validators=[DataRequired(), ])
+    groupname = TextField('Group name', validators=[DataRequired(), ])
+    description = TextAreaField('Description', validators=[DataRequired(), ])
 
 
 class FindPeopleForm(Form):
     email = TextField('Email-Address', validators=[
-        Required(), Email()])
+        DataRequired(), Email()])
 
 
 class AddContactForm(Form):
-    recipient_id = TextField('Persona ID', validators=[Required()])
-    author_id = SelectField('Contact as', validators=[Required()])
+    recipient_id = TextField('Persona ID', validators=[DataRequired()])
+    author_id = SelectField('Contact as', validators=[DataRequired()])
