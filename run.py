4bddfaf3bf22e55fc5153b87cbea3afa38d0f39a
Vincent Ahrend <nichte@gmail.com>
2014-02-19 00:30:27 +0100

    Auto-download model data

--- run.py
+++ run.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
 
 import gevent
+import requests
 
 from web_ui import app, db
 
+from gevent import Greenlet
 from gevent.wsgi import WSGIServer
 from sqlalchemy.exc import OperationalError
 from uuid import uuid4
@@ -14,8 +16,44 @@ from synapse import Synapse
 from astrolab.helpers import repeated_func_schedule
 from astrolab.interestmodel import update
 
+
 def setup_astrolab():
     """Download topic model and schedule model updates"""
+    model_filename = app.config["TOPIC_MODEL"]
+    word_ids_filename = app.config["TOPIC_MODEL_IDS"]
+
+    model_url = app.config["TOPIC_MODEL_UPDATE"]
+    word_ids_url = app.config["TOPIC_MODEL_IDS_UPDATE"]
+
+    try:
+        with open(word_ids_filename):
+            app.logger.debug("Model word ids found")
+    except IOError:
+        app.logger.info("Now downloading model data ids")
+        r = requests.get(word_ids_url, stream=True)
+        with open(word_ids_filename, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=1024):
+                if chunk:
+                    f.write(chunk)
+                    f.flush()
+        app.logger.info("Model data ids downloaded")
+
+    try:
+        with open(model_filename):
+            app.logger.debug("Model data found")
+    except IOError:
+        app.logger.info("Downloading model data")
+        r = requests.get(model_url, stream=True)
+        with open(model_filename, 'wb') as f:
+            i = 0
+            for chunk in r.iter_content(chunk_size=1024):
+                if chunk:
+                    f.write(chunk)
+                    f.flush()
+                    i += 1
+                    if i % (1024 * 5) == 0:
+                        app.logger.info("Downloaded {} MB / 323 MB".format(i / 1024))
+        app.logger.info("Model data downloaded")
 
     repeated_func_schedule(60 * 60, update)
 
@@ -45,7 +83,7 @@ else:
     synapse = Synapse()
 
     # Setup Astrolab
-    gevent.spawn(setup_astrolab())
+    Greenlet.spawn(setup_astrolab)
 
     # Web UI
     if not app.config['NO_UI']:
