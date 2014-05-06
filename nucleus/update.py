339f2a84716471ef6347a913c6dfa425c95afd27
Vincent Ahrend <nichte@gmail.com>
2014-05-06 18:14:38 +0200

    Catch exceptions during update

--- nucleus/update.py
+++ nucleus/update.py
@@ -6,9 +6,12 @@ from web_ui import app as souma_app
 def update_souma():
     """Check for updates and install if available"""
     app = Esky(sys.executable, souma_app.config["UPDATE_URL"])
-    souma_app.logger.info("Running version {}".format(app.active_version))
-    app.auto_update()
-    # try:
-    #     app.auto_update()
-    # except Exception, e:
-    #     souma_app.logger.error("Error updating Souma: {}".format(e))
+    try:
+        app.auto_update()
+
+    # Exception kinds are badly documented for the Esky auto_update method,
+    # that's why I'm using a catch-all except here.
+    # Esky advises for writing custom auto_update routines anyway, which is
+    # probably a good idea before we start public beta testing.
+    except Exception, e:
+        souma_app.logger.error("Error updating Souma: {}".format(e))
