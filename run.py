7b832d7eec021fa7f4e7dd1b788f3170be72f1af
cartisan <leon@die-genossen.de>
2014-05-09 03:12:25 +0200

    refactored app-config from run.py to nucleus

--- run.py
+++ run.py
@@ -4,7 +4,6 @@ import os
 import sys
 import webbrowser
 import semantic_version
-import logging
 import argparse
 
 from web_ui import app, db
@@ -15,11 +14,11 @@ from gevent.event import Event
 from sqlalchemy.exc import OperationalError
 from sys import platform
 from uuid import uuid4
-from Crypto.Hash import SHA256
 
 from astrolab.helpers import setup_astrolab
 from nucleus.set_hosts import test_host_entry, create_new_hosts_file, HOSTSFILE
 from nucleus.models import Souma, Starmap
+from nucleus.helpers import configure_from_args, log_config_info, set_souma_id
 from synapse import Synapse
 from web_ui.helpers import host_kind, compile_less
 
@@ -57,28 +56,9 @@ parser.add_argument('-g',
     help="glia server url")
 
 args = parser.parse_args()
-app.config['NO_UI'] = args.no_ui
-app.config['LOGIN_SERVER'] = args.glia
-
-if args.debug is True:
-    app.config["LOG_LEVEL"] = logging.DEBUG
-    app.config["DEBUG"] = True
-    app.logger.debug("Verbose logs active")
-
-if args.port is not None:
-    app.config['LOCAL_PORT'] = args.port
-    app.config['LOCAL_ADDRESS'] = "{}:{}".format(app.config['LOCAL_HOSTNAME'], args.port)
-    app.config['SYNAPSE_PORT'] = args.port + 50
-    app.config['DATABASE'] = os.path.join(app.config["USER_DATA"], 'souma_{}.db'.format(app.config["LOCAL_PORT"]))
-    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + app.config['DATABASE']
-    app.config["SECRET_KEY_FILE"] = os.path.join(app.config["USER_DATA"], "secret_key_{}.dat".format(args.port))
-    app.config["PASSWORD_HASH_FILE"] = os.path.join(app.config["USER_DATA"], "pw_hash_{}.dat".format(args.port))
-
-# Must occur after lines for args.port
-if args.reset is True:
-    from web_ui.helpers import reset_userdata
-    reset_userdata()
-
+configure_from_args(app, args)
+set_souma_id(app)
+log_config_info(app)
 
 """ patch gevent for py2app """
 if getattr(sys, 'frozen', None) == 'macosx_app':
@@ -105,39 +85,6 @@ if getattr(sys, 'frozen', None) == 'macosx_app':
         __httplib__ = imp.load_module('__httplib__', *imp.find_module('httplib'))
         assert __httplib__ is httplib
 
-""" Load/set secret key """
-try:
-    with open(app.config["SECRET_KEY_FILE"], 'rb') as f:
-        app.config['SECRET_KEY'] = f.read(24)
-except IOError:
-    # Create new secret key
-    app.logger.debug("Creating new secret key")
-    app.config['SECRET_KEY'] = os.urandom(24)
-    with open(app.config["SECRET_KEY_FILE"], 'wb') as f:
-        os.chmod(app.config["SECRET_KEY_FILE"], 0700)
-        f.write(app.config['SECRET_KEY'])
-
-# Generate ID used to identify this machine
-app.config['SOUMA_ID'] = SHA256.new(app.config['SECRET_KEY'] + str(app.config['LOCAL_PORT'])).hexdigest()[:32]
-
-if 'SOUMA_PASSWORD_HASH_{}'.format(app.config['LOCAL_PORT']) in os.environ:
-    app.config['PASSWORD_HASH'] = os.environ['SOUMA_PASSWORD_HASH_{}'.format(app.config["LOCAL_PORT"])]
-else:
-    app.config['PASSWORD_HASH'] = None
-
-
-# Log loaded configuration info
-app.logger.info(
-    "\n".join(["{:=^80}".format(" SOUMA CONFIGURATION "),
-              "{:>12}: {}".format("souma", app.config['SOUMA_ID'][:6]),
-              "{:>12}: {}".format("version", app.config['VERSION']),
-              "{:>12}: {}".format("web ui", "disabled" if app.config['NO_UI'] else app.config['LOCAL_ADDRESS']),
-              "{:>12}: {}:{}".format(
-                  "synapse",
-                  app.config['LOCAL_HOSTNAME'],
-                  app.config['SYNAPSE_PORT']),
-              "{:>12}: {}".format("database", app.config['DATABASE']),
-              "{:>12}: {}".format("glia server", app.config['LOGIN_SERVER'])]))
 
 """ Initialize database """
 start = True
