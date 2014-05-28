d3e698538296666b1dae16926aee72722646f61a
Vincent Ahrend <nichte@gmail.com>
2014-05-29 00:55:33 +0200

    Add Glia SSL support

--- web_ui/default_config.py
+++ web_ui/default_config.py
@@ -59,9 +59,13 @@ LESS_FILENAMES = ["main"]
 
 SYNAPSE_PORT = LOCAL_PORT + 2000
 
-LOGIN_SERVER = "app.souma:24500"
-# Uncomment this to use Heroku server
+# A: Use local test server
+# LOGIN_SERVER = "app.souma.io:24500"
+# LOGIN_SERVER_SSL = False
+
+# B: Use Heroku server
 LOGIN_SERVER = "glia.herokuapp.com"
+LOGIN_SERVER_SSL = True
 
 # Setting this to True will automatically upload all vesicles to Myelin, and
 # enable periodic polling of the Myelin for new Vesicles sent to one of the
