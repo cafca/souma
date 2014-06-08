b54258bd34ac0c001650c3664e33dde54727fb17
monas-gray-matter <rleenings@uos.de>
2014-06-08 00:54:39 -0700

    route and view setup for overview of catalogues

--- reflection/views.py
+++ reflection/views.py
@@ -1,7 +1,9 @@
 from flask import abort, flash, redirect, render_template, request, session, url_for
 from web_ui import app, db, logged_in
+from reflection.models import Catalogue
 
 # reflection views
 @app.route('/catalogue_overview')
-def catalogues():
-	return render_template('view_catalogue.html')
+def catalogue_overview():
+	cs = Catalogue.query
+	return render_template('reflection/view_catalogue.html', catalogues = cs)
