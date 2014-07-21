2b4b1f72b4c5b77dadf4e83d07709156b0dc56cc
Vincent Ahrend <nichte@gmail.com>
2014-07-21 13:13:48 +0200

    Reflection formatting

--- reflection/models.py
+++ reflection/models.py
@@ -1,11 +1,12 @@
-from nucleus.models import Serializable, Persona, Oneup, Star, LinkPlanet
+from nucleus.models import Serializable
 from web_ui import db, app
 from uuid import uuid4
 import os
 import csv
 
+
 class Catalogue(Serializable, db.Model):
-    
+
     __tablename__ = "catalogue"
 
     id = db.Column(db.String(32), primary_key=True)
@@ -16,11 +17,8 @@ class Catalogue(Serializable, db.Model):
     ask_for_all = db.Column(db.Boolean)
 
     interval = db.Column(db.Integer)
-    last_used =db.Column(db.DateTime)
+    last_used = db.Column(db.DateTime)
 
-    #author_id = db.Column(db.String(32), db.ForeignKey('persona.id'))
-    #author = db.relationship('Persona',primaryjoin="Persona.id==Catalogue.author_id")
-    
     questions = db.relationship('CatalogueQuestion',
         primaryjoin="CatalogueQuestion.catalogue_id==Catalogue.id",
         backref=db.backref('catalogue'))
@@ -28,22 +26,20 @@ class Catalogue(Serializable, db.Model):
     activated = db.Column(db.Boolean)
 
     def __repr__(self):
-        return "ID: {}, Name: {}".format(self.id,self.name)
+        return "ID: {}, Name: {}".format(self.id, self.name)
 
     @staticmethod
     def readFromCSV():
+        """Read questionnaire from a csv file"""
+        questionnaire_list = ["eggs.csv", "eggs2.csv", "eggs3.csv", "eggs4.csv", "eggs5.csv"]
 
-
-        questionnaire_list = ["eggs.csv","eggs2.csv","eggs3.csv", "eggs4.csv", "eggs5.csv"]
-    
         for questionnaire_item in questionnaire_list:
-
             catalogue_obj = Catalogue()
-            range_start = 0;
-            range_end = 0;
+            range_start = 0
+            range_end = 0
             range_values = ""
             results = False
-            resultEndLine=0
+            resultEndLine = 0
 
             with open(os.path.join(app.config["USER_DATA"], questionnaire_item), 'rb') as csvfile:
                 spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
@@ -54,15 +50,15 @@ class Catalogue(Serializable, db.Model):
                             catalogue_obj.name = row[1]
                         if line == 1:
                             catalogue_obj.description = row[1].decode('utf-8')
-                            
+
                         if line == 2:
                             range_start = row[1]
                         if line == 3:
                             range_end = row[1]
                         if line == 4:
                             range_values = row[1].decode('utf-8')
-                        if line == 5 :
-                            catalogue_obj.system_name= row[1]
+                        if line == 5:
+                            catalogue_obj.system_name = row[1]
                         if line == 6:
                             if row[1] == 0:
                                 catalogue_obj.ask_for_all = False
@@ -70,20 +66,20 @@ class Catalogue(Serializable, db.Model):
                                 catalogue_obj.ask_for_all = True
                         if line == 7:
                             catalogue_obj.interval = row[1]
-                            
+
                             db.session.add(catalogue_obj)
                             #db.session.commit()
 
                     if line >= 9:
                         if row[0] == "" and row[1] == "":
-                            results= True
-                            resultEndLine = line 
+                            results = True
+                            resultEndLine = line
                     if results and line > resultEndLine:
 
-                            if row[2]=="range":
+                            if row[2] == "range":
                                 question = CatalogueRangeQuestion(range_start, range_end, range_values)
-                            if row[2]=="text":
-                                question= CatalogueTextQuestion(False)
+                            if row[2] == "text":
+                                question = CatalogueTextQuestion(False)
 
                             question.id = uuid4().hex
                             question.index = row[0]
@@ -94,129 +90,117 @@ class Catalogue(Serializable, db.Model):
                             #db.session.commit()
                             #print question
 
-
                 print catalogue_obj
-                db.session.commit()         
+                db.session.commit()
                 #print row[0]
                 #print ', '.join(row)
-        
+
 
 class CatalogueQuestion(Serializable, db.Model):
+    """Model a generic question"""
+    __tablename__ = "catalogue_question"
 
-    __tablename__ ="catalogue_question"
     id = db.Column(db.String(32), primary_key=True)
-    question_text=db.Column(db.Text)
+    question_text = db.Column(db.Text)
     index = db.Column(db.Integer)
-
 
     catalogue_id = db.Column(db.String(32), db.ForeignKey('catalogue.id'))
     answers = db.relationship('CatalogueAnswer',
         primaryjoin="CatalogueAnswer.question_id==CatalogueQuestion.id",
         backref=db.backref('question'))
 
-
     identifier = db.Column(db.String(50))
 
-    def __init__(self,identifier):
-        self.type=identifier
+    def __init__(self, identifier):
+        self.type = identifier
 
     __mapper_args__ = {
-        'polymorphic_identity':'catalogue_question',
+        'polymorphic_identity': 'catalogue_question',
         'polymorphic_on': identifier
     }
 
 
 class CatalogueRangeQuestion(CatalogueQuestion):
-
-    __tablename__ ="catalogue_range_question"
+    __tablename__ = "catalogue_range_question"
 
     id = db.Column(db.String, db.ForeignKey('catalogue_question.id'), primary_key=True)
-
     start_value = db.Column(db.Integer)
     end_value = db.Column(db.Integer)
-
-    range_text_values=db.Column(db.String)
+    range_text_values = db.Column(db.String)
 
     def __init__(self, start_value, end_value, rangetextv):
-        CatalogueQuestion.__init__(self,type)
+        CatalogueQuestion.__init__(self, type)
         self.start_value = start_value
         self.end_value = end_value
         self.range_text_values = rangetextv
 
     def __repr__(self):
-        return "Index: {}, Start: {}, End: {}".format(self.index,self.start_value,self.end_value)
+        return "Index: {}, Start: {}, End: {}".format(self.index, self.start_value, self.end_value)
 
     __mapper_args__ = {
-        'polymorphic_identity':'catalogue_range_question'
+        'polymorphic_identity': 'catalogue_range_question'
     }
 
 
 class CatalogueTextQuestion(CatalogueQuestion):
 
-    __tablename__ ="catalogue_text_question"
+    __tablename__ = "catalogue_text_question"
     id = db.Column(db.String, db.ForeignKey('catalogue_question.id'), primary_key=True)
 
-    requires_short_answer=db.Column(db.Boolean)
+    requires_short_answer = db.Column(db.Boolean)
 
     def __init__(self, requires_short_answer):
-        self.requires_short_answer=requires_short_answer
+        self.requires_short_answer = requires_short_answer
 
     __mapper_args__ = {
-        'polymorphic_identity':'catalogue_text_question'
+        'polymorphic_identity': 'catalogue_text_question'
     }
 
 
 class CatalogueAnswer(Serializable, db.Model):
 
-    __tablename__ ="catalogue_answer"
+    __tablename__ = "catalogue_answer"
     id = db.Column(db.String(32), primary_key=True)
-
-    answer_time=db.Column(db.DateTime)
-
+    answer_time = db.Column(db.DateTime)
     question_id = db.Column(db.String(32), db.ForeignKey('catalogue_question.id'))
-    
-    #to identify two answers given together 
+    #to identify two answers given together
     run_id = db.Column(db.String(32))
+    identifier = db.Column(db.String(50))
 
-    identifier= db.Column(db.String(50))
-
-    def __init__(self,identifier):
-        self.type=identifier
+    def __init__(self, identifier):
+        self.type = identifier
 
     __mapper_args__ = {
-        'polymorphic_identity':'catalogue_answer',
+        'polymorphic_identity': 'catalogue_answer',
         'polymorphic_on': identifier
     }
 
+
 class CatalogueRangeAnswer(CatalogueAnswer):
+    __tablename__ = "catalogue_range_answer"
 
-    __tablename__ ="catalogue_range_answer"
     id = db.Column(db.String, db.ForeignKey('catalogue_answer.id'), primary_key=True)
-
-    range_value= db.Column(db.Integer)
-
+    range_value = db.Column(db.Integer)
 
     def __init__(self):
-        CatalogueAnswer.__init__(self,type)
+        CatalogueAnswer.__init__(self, type)
 
     __mapper_args__ = {
-        'polymorphic_identity':'catalogue_range_answer'
+        'polymorphic_identity': 'catalogue_range_answer'
     }
 
 
-""" We need a connection to question type as -requires short answer- is otherwise obsolete """
 class CatalogueTextAnswer(CatalogueAnswer):
+    # We need a connection to question type as -requires short answer- is otherwise obsolete
 
-    __tablename__ ="catalogue_text_answer"
+    __tablename__ = "catalogue_text_answer"
+
     id = db.Column(db.String, db.ForeignKey('catalogue_answer.id'), primary_key=True)
-
-    answer_text= db.Column(db.Text)
+    answer_text = db.Column(db.Text)
 
     def __init__(self):
-        CatalogueAnswer.__init__(self,type)
+        CatalogueAnswer.__init__(self, type)
 
     __mapper_args__ = {
-        'polymorphic_identity':'catalogue_text_answer'
+        'polymorphic_identity': 'catalogue_text_answer'
     }
-
-
--- reflection/views.py
+++ reflection/views.py
@@ -1,46 +1,45 @@
-from flask import abort, escape, flash, redirect, render_template, request, session, url_for
-from web_ui import app, db, logged_in
-from uuid import uuid4
 import datetime
-from reflection.models import *
-from reflection.forms import *
-from sqlalchemy import desc
 import json
 
-# reflection views
+from flask import abort, flash, redirect, render_template, request, url_for
+from web_ui import app, db
+from uuid import uuid4
+from reflection.models import Catalogue, CatalogueAnswer, CatalogueQuestion, CatalogueTextAnswer, CatalogueRangeAnswer
+from reflection.forms import Answer_range_question_form, Answer_text_question_form
+
+
 @app.route('/catalogue_overview')
 def catalogue_overview():
     cs = Catalogue.query
-    return render_template('reflection/view_catalogues.html', catalogues = cs)
+    return render_template('reflection/view_catalogues.html', catalogues=cs)
 
 
 @app.route('/catalogue/<id>/', methods=['GET'])
 def catalogue(id):
     """ Display a catalogue """
-    catalogue = Catalogue.query.filter(Catalogue.id == id).first_or_404()
-    #author = Persona.query.filter_by(id=id)
+    catalogue = Catalogue.query.get(id)
 
-    return render_template('reflection/show_catalogue.html', catalogue=catalogue)
-
+    return render_template('reflection/show_catalogue.html', catalogue=catalogue) or abort(404)
 
 
 @app.route('/activate_catalogue/<id>/', methods=['GET'])
 def activate_catalogue(id):
 
-    catalogue = Catalogue.query.filter(Catalogue.id == id).first_or_404()
+    catalogue = Catalogue.query.get(id)
     catalogue.activated = True
     db.session.commit()
 
-    return redirect(url_for('catalogue_overview'))
+    return redirect(url_for('catalogue_overview')) or abort(404)
+
 
 @app.route('/deactivate_catalogue/<id>/', methods=['GET'])
 def deactivate_catalogue(id):
 
-    catalogue = Catalogue.query.filter(Catalogue.id == id).first_or_404()
+    catalogue = Catalogue.query.get(id)
     catalogue.activated = False
     db.session.commit()
 
-    return redirect(url_for('catalogue_overview'))
+    return redirect(url_for('catalogue_overview')) or abort(404)
 
 
 @app.route('/start_catalogue/<id>/', methods=['GET'])
@@ -49,35 +48,31 @@ def start_catalogue(id):
     catalogue = Catalogue.query.filter(Catalogue.id == id).first_or_404()
     max_index = len(catalogue.questions)
 
-    #find last answered question to continue with next 
-    laq = CatalogueAnswer.query.join(CatalogueQuestion).join(CatalogueQuestion.catalogue).filter(Catalogue.id==catalogue.id).order_by(CatalogueAnswer.answer_time.desc()).first()
+    #find last answered question to continue with next
+    laq = CatalogueAnswer.query.join(CatalogueQuestion).join(CatalogueQuestion.catalogue).filter(Catalogue.id == catalogue.id).order_by(CatalogueAnswer.answer_time.desc()).first()
     if laq is not None:
         if laq.question.index < max_index:
-            return redirect(route_to_next_question(catalogue.id, laq.question.index +1))
+            return redirect(route_to_next_question(catalogue.id, laq.question.index + 1))
         else:
             return redirect(route_to_next_question(catalogue.id, 1))
     else:
         return redirect(route_to_next_question(catalogue.id, 1))
 
 
-@app.route('/answer_text_question/<id>/', methods=['GET','POST'])
+@app.route('/answer_text_question/<id>/', methods=['GET', 'POST'])
 def answer_text_question(id):
     """ Display a form to answer catalogue """
 
     q = CatalogueQuestion.query.filter(CatalogueQuestion.id == id).first_or_404()
-
     form = Answer_text_question_form()
-
     percentage = get_progress_bar_percent(q.catalogue_id, q.index)
-   
-    if form.validate_on_submit():
-        #import pdb; pdb.set_trace()
 
+    if form.validate_on_submit():
         run_uuid = get_run_uuid(q.catalogue_id, q.index)
-      
+
         #neue Antwort anlegen
         answer = CatalogueTextAnswer()
-        answer.id=uuid4().hex
+        answer.id = uuid4().hex
         answer.answer_text = request.form['text']
         answer.answer_time = datetime.datetime.utcnow()
         answer.question_id = q.id
@@ -86,37 +81,33 @@ def answer_text_question(id):
         db.session.commit()
 
         #find next question
-        return redirect(route_to_next_question(q.catalogue_id , q.index +1))
-
-        #flash("New Answer created!")
-        #return redirect('/catalogue_overview')
+        return redirect(route_to_next_question(q.catalogue_id, q.index + 1))
     else:
         return render_template('reflection/answer_text_question.html', question=q, form=form, percentage=percentage)
 
 
-@app.route('/answer_range_question/<id>/', methods=['GET','POST'])
+@app.route('/answer_range_question/<id>/', methods=['GET', 'POST'])
 def answer_range_question(id):
     """ Display a form to answer catalogue """
 
     q = CatalogueQuestion.query.filter(CatalogueQuestion.id == id).first_or_404()
-      
-    form=Answer_range_question_form()
+
+    form = Answer_range_question_form()
     percentage = get_progress_bar_percent(q.catalogue_id, q.index)
+
     #Choices
-    range_choices=[]
-    split_text_values=q.range_text_values.split(',')
-    for i in range(q.start_value,q.end_value+1):
-        range_choices.append([i,split_text_values[i-1]])
+    range_choices = []
+    split_text_values = q.range_text_values.split(',')
+    for i in range(q.start_value, q.end_value + 1):
+        range_choices.append([i, split_text_values[i - 1]])
     form.range_value.choices = range_choices
 
-    
     if form.validate_on_submit():
-     
         run_uuid = get_run_uuid(q.catalogue_id, q.index)
-      
+
         #neue Antwort anlegen
         answer = CatalogueRangeAnswer()
-        answer.id=uuid4().hex
+        answer.id = uuid4().hex
         answer.range_value = request.form['range_value']
         answer.answer_time = datetime.datetime.utcnow()
         answer.question_id = q.id
@@ -125,69 +116,66 @@ def answer_range_question(id):
         db.session.commit()
 
         #find next question
-        return redirect(route_to_next_question(q.catalogue_id , q.index+1))
-
+        return redirect(route_to_next_question(q.catalogue_id, q.index + 1))
     else:
-    
         return render_template('reflection/answer_range_question.html', question=q, form=form, percentage=percentage)
 
 
 def get_run_uuid(cat_id, q_index):
-
     #vorgaenger Antwort finden
     if q_index == 1:
         return uuid4().hex
     else:
-        prev_q= CatalogueQuestion.query.filter(CatalogueQuestion.catalogue_id == cat_id, CatalogueQuestion.index == q_index-1).first()
+        prev_q = CatalogueQuestion.query.filter(CatalogueQuestion.catalogue_id == cat_id, CatalogueQuestion.index == q_index - 1).first()
         if prev_q is not None:
-            prev_a = CatalogueAnswer.query.filter(CatalogueAnswer.question_id==prev_q.id).order_by(CatalogueAnswer.answer_time.desc()).first()
+            prev_a = CatalogueAnswer.query.filter(CatalogueAnswer.question_id == prev_q.id).order_by(CatalogueAnswer.answer_time.desc()).first()
             if prev_a is not None:
                 return prev_a.run_id
                 #Todo: what if he does not find the previous answer?
 
 
 def get_progress_bar_percent(cat_id, question_index):
-    nex_question = CatalogueQuestion.query.filter(CatalogueQuestion.catalogue_id == cat_id,CatalogueQuestion.index == question_index).first()  
+    nex_question = CatalogueQuestion.query.filter(CatalogueQuestion.catalogue_id == cat_id, CatalogueQuestion.index == question_index).first()
     max_index = len(nex_question.catalogue.questions)
-    return round(100 * float(question_index)/float(max_index+1),2)
+    return round(100 * float(question_index) / float(max_index + 1), 2)
+
 
 def route_to_next_question(cat_id, redirect_question_index):
-    nex_question = CatalogueQuestion.query.filter(CatalogueQuestion.catalogue_id == cat_id,CatalogueQuestion.index == redirect_question_index).first()
-    
+    nex_question = CatalogueQuestion.query.filter(CatalogueQuestion.catalogue_id == cat_id, CatalogueQuestion.index == redirect_question_index).first()
+
     if nex_question is not None:
         #flash("New Answer created!")
         if nex_question.identifier == "catalogue_range_question":
-            return url_for('answer_range_question', id= nex_question.id)
-        if nex_question.identifier == "catalogue_text_question" :
-             return url_for('answer_text_question', id= nex_question.id)
+            return url_for('answer_range_question', id=nex_question.id)
+        if nex_question.identifier == "catalogue_text_question":
+            return url_for('answer_text_question', id=nex_question.id)
     else:
         flash("Excellent Job! Questionnaire completely answered.")
         return '/catalogue_overview'
 
+
 @app.route('/show_catalogue_answers/<id>/<index>', methods=['GET'])
 def show_catalouge_answers(id, index):
-
-
     offset_index = int(index)
     catalogue_answers = None
 
-    prev_index = offset_index -1
-    next_index = offset_index +1
+    prev_index = offset_index - 1
+    next_index = offset_index + 1
 
     # correct if first or last item
-    max_runs = CatalogueAnswer.query.join(CatalogueQuestion).join(CatalogueQuestion.catalogue).filter(Catalogue.id==id).group_by(CatalogueAnswer.run_id).order_by(CatalogueAnswer.answer_time.desc()).count()
+    max_runs = CatalogueAnswer.query.join(CatalogueQuestion).join(CatalogueQuestion.catalogue).filter(Catalogue.id == id).group_by(CatalogueAnswer.run_id).order_by(CatalogueAnswer.answer_time.desc()).count()
 
     if offset_index == 0:
         prev_index = -1
-    if offset_index == max_runs -1:
+    if offset_index == max_runs - 1:
         next_index = -1
 
-        
-    catalogue_answer_run_id  = CatalogueAnswer.query.join(CatalogueQuestion).join(CatalogueQuestion.catalogue).filter(Catalogue.id==id).group_by(CatalogueAnswer.run_id).order_by(CatalogueAnswer.answer_time.desc()).offset(offset_index).first()
+    catalogue_answer_run_id = CatalogueAnswer.query.join(CatalogueQuestion).join(CatalogueQuestion.catalogue).filter(Catalogue.id == id).group_by(CatalogueAnswer.run_id).order_by(CatalogueAnswer.answer_time.desc()).offset(offset_index).first()
+
     if catalogue_answer_run_id is not None:
-        catalogue_answers = CatalogueAnswer.query.filter(CatalogueAnswer.run_id==catalogue_answer_run_id.run_id).all()
+        catalogue_answers = CatalogueAnswer.query.filter(CatalogueAnswer.run_id == catalogue_answer_run_id.run_id).all()
     else:
-        catalogue_answers = CatalogueAnswer.query.join(CatalogueQuestion).join(CatalogueQuestion.catalogue).filter(Catalogue.id==id).all()
+        catalogue_answers = CatalogueAnswer.query.join(CatalogueQuestion).join(CatalogueQuestion.catalogue).filter(Catalogue.id == id).all()
 
     #import pdb; pdb.set_trace()
     # if index > len(catalogue_answers)-1:
@@ -195,21 +183,19 @@ def show_catalouge_answers(id, index):
     #     return 404
     # else:
 
-    #Falls der Fragebogen noch nicht beantwortet wurde.
-    if len(catalogue_answers) == 0 :
+    # Questionnaire has not been answered before
+    if len(catalogue_answers) == 0:
         flash("No answers to show. Answer questionnaire first!")
         return redirect(url_for('catalogue_overview'))
     else:
-        return render_template('reflection/show_catalogue_answers.html', answers = catalogue_answers, cat_id=id, prev_index=prev_index, next_index=next_index)
+        return render_template('reflection/show_catalogue_answers.html', answers=catalogue_answers, cat_id=id, prev_index=prev_index, next_index=next_index)
+
 
 @app.route('/show_graph/<id>', methods=['GET'])
 def show_graph(id):
+    range_questions = CatalogueQuestion.query.filter(CatalogueQuestion.catalogue_id == id, CatalogueQuestion.identifier == "catalogue_range_question").all()
 
-
-    range_questions = CatalogueQuestion.query.filter(CatalogueQuestion.catalogue_id==id,CatalogueQuestion.identifier=="catalogue_range_question").all()
-    #import pdb; pdb.set_trace()
-    if len(range_questions)>0:
-
+    if len(range_questions) > 0:
         question = range_questions[0]
         graph_title = question.catalogue.name
         yAxis_title = question.question_text
@@ -217,67 +203,61 @@ def show_graph(id):
         # Beschriftung der Y Axen Abschnitte
         plot_band_string = '['
         range_value_texts = question.range_text_values.split(',')
-      
+
         for range_index in range(0, len(range_value_texts)):
-            range_text_dict=dict()
-            range_text_dict["from"]=range_index
-            range_text_dict["to"]=range_index +1
+            range_text_dict = dict()
+            range_text_dict["from"] = range_index
+            range_text_dict["to"] = range_index + 1
             if range_index % 2 == 0:
-                range_text_dict["color"]="#fff"
+                range_text_dict["color"] = "#fff"
             else:
-                range_text_dict["color"]="#f1f1f1"
-            range_text_dict["label"]={"text": range_value_texts[range_index].encode('utf-8'), "style": {"color":'#606060'}}
+                range_text_dict["color"] = "#f1f1f1"
+            range_text_dict["label"] = {"text": range_value_texts[range_index].encode('utf-8'), "style": {"color": '#606060'}}
 
             plot_band_string += json.dumps(range_text_dict) + ','
 
         plot_band_string += ']'
 
         #Data:
-
         json_string = "["
         x_data_set = set()
-
         count = 0
 
         for q in range_questions:
 
-            range_answers = CatalogueAnswer.query.filter(CatalogueAnswer.question_id==q.id).all()
-            
+            range_answers = CatalogueAnswer.query.filter(CatalogueAnswer.question_id == q.id).all()
+
             if range_answers is not None:
 
-                data_list = []
+                # data_list = []
                 json_string += '{'
                 json_string += 'name: '
                 json_string += "'"
                 json_string += q.question_text
                 json_string += "', data: ["
-                
+
                 comma_count = 0
                 for a in range_answers:
-                     json_string += "[Date.UTC({}, {}, {}), {}]".format(a.answer_time.year, a.answer_time.month-1, a.answer_time.day, a.range_value)          
-                     if comma_count < len(range_answers)-1:
+                    json_string += "[Date.UTC({}, {}, {}), {}]".format(a.answer_time.year, a.answer_time.month - 1, a.answer_time.day, a.range_value)
+                    if comma_count < len(range_answers) - 1:
                         json_string += ","
-                     comma_count += 1
+                    comma_count += 1
 
                 json_string += "]}"
                 #json_string += json.dumps(data_dict)
-                if count < len(range_questions)-1: 
+                if count < len(range_questions) - 1:
                     json_string += ","
                 count = count + 1
-                #pdb.set_trace()
-            
-            
+
         json_string += "]"
-        final_string= json_string
-                        
-        x_data_list=[]
+        final_string = json_string
+
+        x_data_list = []
         for item in x_data_set:
-            x_data_list.append(item) 
-    
+            x_data_list.append(item)
+
         return render_template('reflection/show_diagramm.html', graph_title=graph_title, yAxis_title=yAxis_title, series=final_string, x_values=x_data_list, plot_band_values=plot_band_string)
 
     else:
         flash('No Range Questions for Plotting a Graph.')
         return redirect(url_for('catalogue_overview'))
-
-   
